80e14154426694f9d1f4d7cbad03c59baf91cad6
staurus86 <55583713+staurus86@users.noreply.github.com>
2023-02-01 15:51:31 +0300

    Add files via upload

---
+++ bot2.py
@@ -0,0 +1,21 @@
+import openai
+import telebot
+
+openai.api_key = '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'
+bot = telebot.TeleBot("6197915948:AAFKPBzqEAT6enKOK4A4kC8O7XMZ2tyIoPA")
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=message.text,
+        max_tokens=1024,
+        top_p=1.0,
+        temperature=0.5,
+        frequency_penalty=0.5,
+        presence_penalty=0.0,
+    )
+
+    bot.send_message(chat_id=message.from_user.id, text=response['choices'][0]['text'])
+
+bot.polling()
